d941b47538bd103900a79993bf47807190a1399e
Yash Priyam <raunakrajput2411@gmail.com>
2026-03-19 12:40:36 +0530

    Create script2_package_inspector.sh

---
+++ script2_package_inspector.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+# Purpose: Check if Python is installed and show details
+
+PACKAGE="python3"   # The package we want to check
+
+# First check if the package is installed
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    # Print version information
+    dpkg -l | grep $PACKAGE | awk '{print "Version: "$3}'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+# Print a short philosophy note depending on the package
+case $PACKAGE in
+    python3) echo "Python: a language shaped entirely by community." ;;
+    httpd)   echo "Apache: the web server that built the open internet." ;;
+    mysql)   echo "MySQL: open source at the heart of millions of apps." ;;
+    vlc)     echo "VLC: plays anything, built by students in Paris." ;;
+    *)       echo "Unknown package." ;;
+esac
